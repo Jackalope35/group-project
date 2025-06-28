c0d6fde3350f830817309a5211a7ed1c928874c9
Jackalope35 <hrgarcia126@gmail.com>
2025-06-27 19:43:11 -0700

    Clean up leftover project_1 folder

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import logo from './HECOR.png';
+import './App.css';
+import wiggum from './wiggum.gif';
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          <code>Check me out!</code> I'm learnding
+        </p>
+        <img 
+  src={wiggum} 
+  alt="ralph wiggum gif" 
+  style={{ height: '160px', margin: '10px 0' }} 
+/>
+
+        <a
+          className="App-link"
+          href="https://youtube.com/shorts/Dnk0U6_L4y0?si=Ur59dlRyAa5a2zXq"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React by Clicking Here! {/* link text*/}
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;

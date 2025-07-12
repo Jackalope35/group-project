dabe346a027747f8dce7270c34b68d9f5b486f3c
Jackalope35 <hrgarcia126@gmail.com>
2025-07-11 22:12:04 -0700

    Added link to pearl's html page

--- src/App.js
+++ src/App.js
@@ -6,17 +6,22 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        {/* Logo image */}
         <img src={logo} className="App-logo" alt="logo" />
+
+        {/* Text below logo */}
         <p>
           <code>Check me out!</code> I'm learnding
         </p>
 
+        {/* Wiggum gif */}
         <img 
           src={wiggum} 
           alt="ralph wiggum gif" 
           style={{ height: '160px', margin: '10px 0' }} 
         />
 
+        {/* YouTube link */}
         <a
           className="App-link"
           href="https://youtube.com/shorts/Dnk0U6_L4y0?si=Ur59dlRyAa5a2zXq"
@@ -26,16 +31,17 @@ function App() {
           Learn React by Clicking Here!
         </a>
 
-        {/* 🔗 Link to Pearl's work */}
-        <a 
-          href="/index.html" 
-          target="_blank" 
-          rel="noopener noreferrer"
-        >
-          <button style={{ marginTop: '20px' }}>
-            Click here for Pearl's work
-          </button>
-        </a>
+        {/* Link to Pearl's work */}
+        <a
+  href={`${process.env.PUBLIC_URL}/pearl.html`}
+  target="_blank"
+  rel="noopener noreferrer"
+>
+  <button style={{ marginTop: '20px' }}>
+    Click here for Pearl's work
+  </button>
+</a>
+
       </header>
     </div>
   );
